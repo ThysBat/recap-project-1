2c62a368e77052c7a3d32bca28698173a2e47457
David Illies <dav.illies@web.de>
2023-05-08 10:40:34 +0200

    add comments to form.js

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -1,5 +1,6 @@
 const bookmarks = document.querySelectorAll('[data-js="bookmark"]');
 
+// toggle bookmark icon for all bookmarks
 for (const bookmark of bookmarks) {
   bookmark.addEventListener("click", (event) => {
     bookmark.classList.toggle("--bookmarked");
--- js/form.js
+++ js/form.js
@@ -1,15 +1,21 @@
+// fetch needed html elements
 const form = document.querySelector('[data-js="new-question-form"]');
 const main = form.parentElement;
 
+// add event listener to the new question form submit button
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  // extract form data
   const formData = new FormData(event.target);
   addNewQuestionCard(formData);
-  //   event.target.reset();
+
+  // reset the form and its focus
+  event.target.reset();
   event.target.elements[0].focus();
 });
 
+// create and append the new question card
 function addNewQuestionCard(formData) {
   const data = Object.fromEntries(formData);
   const questionCardComponent = createQuestionCardComponent(data);
@@ -17,7 +23,9 @@ function addNewQuestionCard(formData) {
   main.append(questionCardComponent);
 }
 
+// create the question card elements, attributes, classes and content
 function createQuestionCardComponent(data) {
+  // create html elements
   const questionCardElement = document.createElement("section");
   const imageWrapperElement = document.createElement("div");
   const bookmarkElement = document.createElement("img");
@@ -30,25 +38,31 @@ function createQuestionCardComponent(data) {
   questionCardElement.classList.add("question-card", "section-sizing");
   imageWrapperElement.classList.add("question-card__image-wrapper");
 
+  // set up bookmark element
   bookmarkElement.classList.add("question-card__bookmark");
   bookmarkElement.setAttribute("data-js", "bookmark");
 
+  // set up question element
   questionElement.classList.add("question-card__question");
   questionElement.textContent = data["question-input"];
 
+  // set up submit button element
   buttonElement.classList.add("question-card__button", "show-answer-button");
   buttonElement.type = "button";
   buttonElement.setAttribute("data-js", "answer-button");
   buttonElement.textContent = "Show Answer";
 
+  // set up answer element
   answerElement.classList.add("question-card__answer", "--display--none");
   answerElement.setAttribute("data-js", "answer");
   answerElement.textContent = data["answer-input"];
 
+  // set up tag element
   tagListElement.classList.add("question-card__tag-container");
   tagItemElement.classList.add("question-card__tag");
   tagItemElement.textContent = "#" + data["tag-input"];
 
+  // append every element to its corresponding parent
   questionCardElement.append(imageWrapperElement);
   imageWrapperElement.append(bookmarkElement);
   questionCardElement.append(questionElement);
